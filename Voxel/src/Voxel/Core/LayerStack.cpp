6b63bb203f438b3d17b25202dbe3e8d47a372f3c
Ben Püschel <ben.pueschel2004@gmail.com>
2021-06-23 14:37:19 +0200

    Detach Layers before deleting them

--- Voxel/src/Voxel/Core/LayerStack.cpp
+++ Voxel/src/Voxel/Core/LayerStack.cpp
@@ -8,7 +8,10 @@ namespace Voxel {
 	LayerStack::~LayerStack()
 	{
 		for (Layer* layer : m_Layers)
+		{
+			layer->OnDetach();
 			delete layer;
+		}
 	}
 
 	void LayerStack::PushLayer(Layer* layer)
