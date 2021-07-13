2bc56366b5432617cb958cee8c109c7b35dca8a4
Ben Püschel <ben.pueschel2004@gmail.com>
2021-07-13 18:57:00 +0200

    Create seperate header file for platform detection

--- Voxel/src/Voxel/Core/Core.h
+++ Voxel/src/Voxel/Core/Core.h
@@ -1,48 +1,10 @@
 #pragma once
 
 #include <memory>
-#include "Voxel/Logging/Logger.h"
+#include "PlatformDetection.h"
+#include "Voxel/Debug/Assert.h"
 
 #define BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
-
-#ifdef _WIN32
-	#ifdef _WIN64
-		#define VOXEL_PLATFORM_WINDOWS
-	#else
-		#error "x86 Builds are not supported!"
-	#endif
-
-#elif defined(__APPLE__) || defined(__MACH__)
-	#include <TargetConditionals.h>
-	/* TARGET_OS_MAC exists on all the platforms
-	 * so we must check all of them (in this order)
-	 * to ensure that we're running on MAC
-	 * and not some other Apple platform */
-	#if TARGET_IPHONE_SIMULATOR == 1
-		#error "IOS simulator is not supported!"
-	#elif TARGET_OS_IPHONE == 1
-		#define VOXEL_PLATFORM_IOS
-		#error "IOS is not supported!"
-	#elif TARGET_OS_MAC == 1
-		#define VOXEL_PLATFORM_MACOS
-		#error "MacOS is not supported!"
-	#else
-		#define VOXEL_API __declspec(dllimport)
-		#error "Unknown Apple platform!"
-	#endif
-
-/* We also have to check __ANDROID__ before __linux__
- * since android is based on the linux kernel
- * it has __linux__ defined */
-#elif defined(__ANDROID__)
-	#define VOXEL_PLATFORM_ANDROID
-	#error "Android is not supported!"
-#elif defined(__linux__)
-	#define VOXEL_PLATFORM_LINUX
-	#error "Linux is not supported!"
-#else
-	#error "Unkown platform!"
-#endif
 
 // DLL support
 #ifdef VOXEL_PLATFORM_WINDOWS
